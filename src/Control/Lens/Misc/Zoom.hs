5021b5f4a7245eb84c00f543f10e1a20059a842f
Louis Pan <louis@pan.me>
2019-04-26 06:27:44 +1000

    zoomUnder uses explicit iso

--- src/Control/Lens/Misc/Zoom.hs
+++ src/Control/Lens/Misc/Zoom.hs
@@ -7,19 +7,12 @@ import Control.Lens
 
 -- | 'magnifying'ing with a Traversal constrains the result type @a@ to be a 'Monoid'.
 -- This allows temporarily wrapping @a@ into a 'Monoid' when 'magnifying'
--- @Control.Newtype.underF Sum f ≡ mapping (_Unwrapping Sum) %~ f@
--- Control.Lens.Wrapped (4.17) doc is wrong and used '_Wrapping'
--- '_Unwrapping' can be used with 'Control.Newtype.Generics' instances too.
-magnifyUnder :: (Rewrapped wrapper wrapper, Magnify m m' s t, Unwrapped wrapper ~ a)
-    => (a -> wrapper) -> LensLike' (Magnified m wrapper) t s -> m a -> m' a
-magnifyUnder wrapper lns = mapping (_Unwrapping wrapper) %~ (magnify lns)
+-- @Control.Newtype.underF Sum f ≡ mapping (iso Sum getSum) %~ f@
+magnifyUnder :: Magnify m m' s t => AnIso' o n -> LensLike' (Magnified m n) t s -> m o -> m' o
+magnifyUnder wrapperIso lns = mapping wrapperIso %~ (magnify lns)
 
 -- | 'zoom'ing with a Traversal constrains the result type @a@ to be a 'Monoid'.
 -- This allows temporarily wrapping @a@ into a 'Monoid'.
--- @Control.Newtype.underF Sum f ≡ mapping (_Unwrapping Sum) %~ f@
--- Control.Lens.Wrapped (4.17) doc is wrong and used '_Wrapping'
--- @zoomUnder wrapper lns = Control.Newtype.Generics.underF wrapper (zoom lns)@
--- '_Unwrapping' can be used with 'Control.Newtype.Generics' instances too.
-zoomUnder :: (Rewrapped wrapper wrapper, Zoom m m' s t, Unwrapped wrapper ~ a)
-    => (a -> wrapper) -> LensLike' (Zoomed m wrapper) t s -> m a -> m' a
-zoomUnder wrapper lns = mapping (_Unwrapping wrapper) %~ (zoom lns)
+-- @Control.Newtype.underF Sum f ≡ mapping (iso Sum getSum) %~ f@
+zoomUnder :: Zoom m m' s t => AnIso' o n -> LensLike' (Zoomed m n) t s -> m o -> m' o
+zoomUnder wrapperIso lns = mapping wrapperIso %~ (zoom lns)
