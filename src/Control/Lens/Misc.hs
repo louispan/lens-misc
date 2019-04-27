35ea1fd88a4208cd01732a08d3ac5704e758e61a
Louis Pan <louis@pan.me>
2019-04-28 06:41:36 +1000

    Remove Ixed

--- src/Control/Lens/Misc.hs
+++ src/Control/Lens/Misc.hs
@@ -1,11 +1,9 @@
 module Control.Lens.Misc
-    ( module Control.Lens.Misc.Ixed
-    , module Control.Lens.Misc.Tagged
+    ( module Control.Lens.Misc.Tagged
     , module Control.Lens.Misc.TH
     , module Control.Lens.Misc.Zoom
     ) where
 
-import Control.Lens.Misc.Ixed
 import Control.Lens.Misc.Tagged
 import Control.Lens.Misc.TH
 import Control.Lens.Misc.Zoom
--- src/Control/Lens/Misc/Ixed.hs
+++
@@ -1,8 +0,0 @@
-{-# LANGUAGE FlexibleContexts #-}
-
-module Control.Lens.Misc.Ixed where
-
-import Control.Lens
-
-findOrInsert :: (At t, Eq (IxValue t)) => Index t -> IxValue t -> t -> (IxValue t, t)
-findOrInsert k v = at k . non v <%~ id
