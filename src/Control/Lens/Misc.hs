4e17ff875b7cb75459274519033fddcc53e8c5f9
Louis Pan <louis@pan.me>
2019-10-14 20:03:50 +1100

    LICENSE, removed stack.yaml

--- src/Control/Lens/Misc.hs
+++ src/Control/Lens/Misc.hs
@@ -3,8 +3,10 @@ module Control.Lens.Misc
     , module Control.Lens.TH.Misc
     , module Control.Lens.Zoom.Misc
     , module Data.Map.Lens.Misc
+    , module Control.Lens
     ) where
 
+import Control.Lens
 import Control.Lens.Tagged.Misc
 import Control.Lens.TH.Misc
 import Control.Lens.Zoom.Misc
