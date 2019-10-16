b5b19414a5ae35cfe3dc174f434721141ca20eda
Louis Pan <louis@pan.me>
2019-10-16 20:41:28 +1100

    added Getting' type synonym

--- src/Control/Lens/Misc.hs
+++ src/Control/Lens/Misc.hs
@@ -4,6 +4,7 @@ module Control.Lens.Misc
     , module Control.Lens.Zoom.Misc
     , module Data.Map.Lens.Misc
     , module Control.Lens
+    , Getting'
     ) where
 
 import Control.Lens
@@ -11,3 +12,5 @@ import Control.Lens.Tagged.Misc
 import Control.Lens.TH.Misc
 import Control.Lens.Zoom.Misc
 import Data.Map.Lens.Misc
+
+type Getting' s a = Getting a s a
